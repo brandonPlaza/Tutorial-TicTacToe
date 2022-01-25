68fdf68356e09b34f1ef40053b1174aaa73ab15d
brandonPlaza <plazab@sheridancollege.ca>
2022-01-25 18:07:18 -0500

    * .gitignore: First tutorial
    * TicTacToe2.sln:
    * App.xaml:
    * App.xaml.cs:
    * MainPage.xaml:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * TicTacToe2.csproj:
    * MainActivity.cs:
    * AboutAssets.txt:
    * TicTacToe2.Android.csproj:
    * AssemblyInfo.cs:
    * colors.xml:
    * styles.xml:
    * AboutResources.txt:
    * icon.png:
    * AndroidManifest.xml:
    * icon.png:
    * Resource.designer.cs:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * icon_round.xml:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ TicTacToe2/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TicTacToe2
+{
+    public partial class MainPage : ContentPage
+    {
+        int player = 1;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            if (((Button)sender).Text != "") return;
+
+            if (player == 1)
+            {
+                ((Button)sender).Text = "X";
+                player = 2;
+            }
+            else
+            {
+                ((Button)sender).Text = "O";
+                player = 1;
+            }
+        }
+
+
+    }
+}
